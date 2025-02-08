06eab7ae9c66b0ba05a25d9393dbcf86d93da2dd
Deep <deeppatel223204@gmail.com>
2025-02-08 16:11:08 +0530

    deep is best

--- frontend/app/(tabs)/account.jsx
+++ frontend/app/(tabs)/account.jsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 const Account = () => {
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-2xl font-bold">Welcome to App</Text>
+      <Text className="text-2xl font-bold">hellowrodl</Text>
       <Text className="text-lg mt-4">Your adventure starts here!</Text>
     </View>
   );
