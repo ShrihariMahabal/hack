9bb40b74603fbadb9a17e16099f5bc04a14d468d
Shrihari Mahabal <shrihari.mahabal22@spit.ac.in>
2025-02-08 14:40:45 +0530

    first commit

---
+++ app/(tabs)/travel/_layout.jsx
@@ -0,0 +1,50 @@
+import { Stack } from "expo-router";
+
+export default function TravelLayout() {
+    return (
+        <Stack>
+            <Stack.Screen 
+                name="index" 
+                options={{
+                    headerTitle: 'Where To?',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        height: 80,
+                    },
+                    headerTitleStyle: {
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                    },
+                }} 
+            />
+            <Stack.Screen 
+                name="routescreen" 
+                options={{
+                    headerTitle: 'Available Routes',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        height: 80,
+                    },
+                    headerTitleStyle: {
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                    },
+                }} 
+            />
+            <Stack.Screen 
+                name="mapscreen" 
+                options={{
+                    headerTitle: 'Your Route',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        height: 80,
+                    },
+                    headerTitleStyle: {
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                    },
+                }} 
+            />
+        </Stack>
+    );
+}
---
+++ app/(tabs)/travel/routescreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Routes = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-bold">Welcome to App</Text>
+      <Text className="text-lg mt-4">Your adventure starts here!</Text>
+    </View>
+  );
+};
+
+export default Routes;
