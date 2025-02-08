5f3aec26285803114ef258a966415d7bcb7f1285
Deep <deeppatel223204@gmail.com>
2025-02-08 21:36:23 +0530

    ok

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React, { useEffect } from "react";
+
+// // Firebase Configuration
+
+// // Initialize Firebase only once
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
 
 export default function App() {
+  //   useEffect(() => {
+  //     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+  //       console.log("Received message:", remoteMessage);
+  //     });
+
+  //     return () => unsubscribe();
+  //   }, []);
+
   return (
-    <View className='flex h-full justify-center items-center'>
-      <Text className='text-blue-800'>Open up App.js to start working on your app!</Text>
-      <Link href='/home'>Go to Home Page</Link>
-      {/* <Link href='/routescreen'>Go to Home Page</Link> */}
-      <StatusBar backgroundColor='#000000' style='auto' />
+    <View className="flex h-full justify-center items-center">
+      <Stack />
+      <Text className="text-blue-800">
+        Open up App.js to start working on your app!
+      </Text>
+      <Link href="/home">Go to Home Page</Link>
+      <StatusBar backgroundColor="#000000" style="auto" />
     </View>
-  )
+  );
 }
